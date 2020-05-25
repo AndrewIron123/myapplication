c3cdcef6414122e932a58e03bc961443e2173819
Andrzej Stal <andrzejstal1991@gmail.com>
2020-05-25 21:38:57 +0200

    fix: Posty - dodanie podstawowej tabeli z lista postow

--- src/Pages/Posts/Posts.jsx
+++ src/Pages/Posts/Posts.jsx
@@ -1,11 +1,43 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { postsService } from './../../Services/postsService';
+import { Table } from 'antd';
 
 class Posts extends Component {
+    state = {
+        posts: []
+    }
+    componentDidMount() {
+        postsService.getPosts()
+            .then(payload => {
+                if (payload.data && Array.isArray(payload.data)) {
+                    this.setState({ posts: payload.data });
+                }
+            })
+    }
+
     render() {
+        const { posts } = this.state;
+        const columns = [
+            {
+                title: 'ID',
+                dataIndex: 'id',
+                key: 'id'
+            },
+            {
+                title: 'Tytuł',
+                dataIndex: 'title',
+                key: 'title',
+            },
+            {
+                title: 'Treść',
+                dataIndex: 'body',
+                key: 'body',
+            },
+        ];
         return (
             <div className="pages__posts">
-                Posts
+                <Table columns={columns} dataSource={posts} />
             </div>
         );
     }
--- src/Services/postsService.js
+++ src/Services/postsService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const postService = {
+export const postsService = {
     getPosts,
     getPost
 }
